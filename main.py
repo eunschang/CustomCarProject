69c997b1ee9e596f902e03d40fe67230d16dff67
ochang2 <27406456-ochang2@users.noreply.replit.com>
2023-10-12 23:53:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,91 @@
+#Greet the user
+print("-------------------------------------")
+print("Welcome to the Car Customization Form")
+print("-------------------------------------")
+print()
+
+#Ask the user for their name
+print("1. Enter Your First and Last Name")
+print()
+firstname = input("First Name: ")
+lastname = input("Last Name: ")
+print()
+
+#Ask the user for their phone number
+print("2. Enter Your Phone Number")
+print()
+phone = input("Phone Number: ")
+print()
+
+#Ask which car the user would like to select
+print("3. Selecting a Car to Customize")
+print()
+print("Select from the Following Options")
+print("A. 2016 Hyundai Genesis")
+print("B. 2021 Mazda MX-5")
+print("C. 2024 Toyota RAV-4 Hybrid")
+print("D. 2024 Nissan Altima")
+print()
+model = input("Your Answer: ")
+print()
+
+#Ask the user if they want tinted windows
+print("4. Windows: Would You Like Tinted Windows?")
+print()
+print("Type Yes or No")
+print("Yes")
+print("No")
+windows = input("Your Answer: ")
+print()
+
+#Ask the user what exterior color they want
+print("5. Exterior Color")
+print()
+print("Select From the Following Options")
+print("A. Sleek Black")
+print("B. Dark Graphite")
+print("C. Chrome White")
+print("D. Midnight Blue")
+exterior = input("Your Answer: ")
+print()
+
+#Ask the user what color interior they want
+print("6. Interior Color")
+print()
+print("Select From the Following Options")
+print("A. Black Leather")
+print("B. Ivory Leather")
+print("C. Grey Leather")
+print("D. Red Leather")
+interior = input("Your Answer: ")
+print()
+
+#Ask the user if they want a custom license plate
+print("7. License Plate: Would You Like a Custom License Plate Message?")
+print ()
+print("Type Yes or No")
+print("Yes")
+print("No")
+license = input("Your Answer:")
+print()
+print("If yes, enter your custom license plate message below")
+print("Note: Must be at least 6 characters, but no longer than 7 characters")
+ifyes = input("Your Answer")
+print()
+print()
+print("Thank You for Completing This Form. You Will Receive a Confirmation Text Shortly.")
+
+#Display a summary of the user's answers
+print("--------------------------------------------")
+print("Customization Summary")
+print("--------------------------------------------")
+print()
+print("Name: ")
+print("Phone Number:")
+print("Car Model: ")
+print("Windows:")
+print("Exterior Color: ")
+print("Interior Color:")
+print("License Plate: ")
+
+      
